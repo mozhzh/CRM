fd500b6aa94e6c0dfaaecd666225771af966aad7
zitttz <zitttz@gmail.com>
2016-12-13 21:40:32 +0300

    add first js file

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+function clickFindButton() {
+    document.getElementById('findFormLabel').style.display = "block";
+    document.getElementById('findForm').style.display = "none";
+}
+
+function clickFindFormLabel() {
+    document.getElementById('findForm').style.display = "block";
+    document.getElementById('findFormLabel').style.display = "none";
+}
